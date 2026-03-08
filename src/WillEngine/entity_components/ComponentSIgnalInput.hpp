45135ba617122dc49b133ecbf9caf46f3684635e
Andrei Gramakov <mail@agramakov.me>
2026-03-08 11:52:07 +0100

    wip

---
+++ src/WillEngine/entity_components/ComponentSIgnalInput.hpp
@@ -0,0 +1,22 @@
+// *************************************************************************
+//
+// Copyright (c) 2026 Andrei Gramakov. All rights reserved.
+//
+// This file is licensed under the terms of the MIT license.
+// For a copy, see: https://opensource.org/licenses/MIT
+//
+// site:    https://agramakov.me
+// e-mail:  mail@agramakov.me
+//
+// *************************************************************************
+
+#pragma once
+
+#include <glm/vec2.hpp>
+
+
+namespace will_engine {
+struct ComponentSignalInput {
+    glm::vec2 input_keyboard;
+};
+} //
